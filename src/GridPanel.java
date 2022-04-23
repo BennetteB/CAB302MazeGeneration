e960359051539f387bfe7c1ca48c3cc82067a4e8
BennetteB <bennette.benoy@outlook.com>
2022-04-23 11:24:58 +1000

    add GridPanel class

---
+++ src/GridPanel.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Creates the Grid Panel with editable cells for Maze construction
+ */
+public class GridPanel extends JPanel {
+    private int mazeWidth;
+    private int mazeHeight;
+
+    public GridPanel() {
+        super();
+    }
+
+    /**
+     * Creates an Empty Grid
+     * @param width Number of cells wide
+     * @param height Number of cells high
+     */
+    protected void CreateGrid(int width, int height) {
+
+    }
+}
