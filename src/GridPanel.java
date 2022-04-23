c0e6f4ce6de1f37df6da21273c3fcca29497e719
BennetteB <bennette.benoy@outlook.com>
2022-04-23 15:58:18 +1000

    add create grid functionality to CreateGrid()

--- src/GridPanel.java
+++ src/GridPanel.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
 /**
  * Creates the Grid Panel with editable cells for Maze construction
@@ -7,13 +10,17 @@ import java.awt.*;
 public class GridPanel extends JPanel {
     enum Position {HORIZONTAL,VERTICAl}
     private GridBagConstraints cst;
-    private int mazeWidth;
-    private int mazeHeight;
+    private int sizeMultiplier = 5;
+    private ArrayList<JButton> btnList = new ArrayList<JButton>();
 
     public GridPanel() {
         super();
         setLayout(new GridBagLayout());
         cst = new GridBagConstraints();
+        cst.weightx = 0;
+        cst.weighty = 0;
+        cst.anchor = GridBagConstraints.CENTER;
+        cst.fill = GridBagConstraints.NONE;
     }
 
     /**
@@ -22,7 +29,49 @@ public class GridPanel extends JPanel {
      * @param height Number of cells high
      */
     protected void CreateGrid(int width, int height) {
-
+        int x = 0;
+        int y = 0;
+        for(int i = 0; i < (height * 2) + 1; i++) {
+            for(int j = 0; j < (width * 2) + 1; j++) {
+                if(i % 2 == 0) {
+                    if(j == 0){
+                        continue;
+                    }
+                    cst.gridx = x;
+                    cst.gridy = y;
+                    cst.gridwidth = 14;
+                    cst.gridheight = 2;
+                    add(createWall(Position.HORIZONTAL),cst);
+                    x += 12;
+                    j += 1;
+                }
+                else{
+                    if(j % 2 == 0) {
+                        cst.gridx = x;
+                        cst.gridy = y - 2;
+                        cst.gridwidth = 2;
+                        cst.gridheight = 14;
+                        add(createWall(Position.VERTICAl),cst);
+                        x += 2;
+                    }
+                    else {
+                        cst.gridx = x;
+                        cst.gridy = y;
+                        cst.gridwidth = 10;
+                        cst.gridheight = 10;
+                        add(createCell(),cst);
+                        x += 10;
+                    }
+                }
+            }
+            if(i % 2 == 0) {
+                y += 2;
+            }
+            else {
+                y += 10;
+            }
+            x = 0;
+        }
     }
 
     /**
@@ -31,7 +80,7 @@ public class GridPanel extends JPanel {
      */
     private JPanel createCell() {
         JPanel panel = new JPanel();
-        panel.setPreferredSize(new Dimension(10,10));
+        panel.setPreferredSize(new Dimension(10 * sizeMultiplier,10 * sizeMultiplier));
         return panel;
     }
 
@@ -42,14 +91,48 @@ public class GridPanel extends JPanel {
      */
     private JButton createWall(Position position) {
         JButton btn = new JButton();
+        btn.setOpaque(false);
+        btn.setBackground(Color.BLACK);
+        btn.addMouseListener(new MouseListener() {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            public void mouseEntered(MouseEvent e) {
+                for(int i = 0; i < btnList.size(); i++) {
+                    if(e.getSource() == btnList.get(i)) {
+                        btnList.get(i).setBackground(Color.RED);
+                        repaint();
+                    }
+                }
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                e.getComponent().setBackground(Color.WHITE);
+            }
+        });
         switch (position) {
             case HORIZONTAL:
-                btn.setPreferredSize(new Dimension(10,2));
+                btn.setPreferredSize(new Dimension(14 * sizeMultiplier,2 * sizeMultiplier));
                 break;
             case VERTICAl:
-                btn.setPreferredSize(new Dimension(2,10));
+                btn.setPreferredSize(new Dimension(2 * sizeMultiplier,14 * sizeMultiplier));
                 break;
         }
-        return btn;
+        btnList.add(btn);
+        return btnList.get(btnList.size() - 1);
     }
 }
