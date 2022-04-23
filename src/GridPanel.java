eff7d4968b90c46bf3110d4585836907109143a8
BennetteB <bennette.benoy@outlook.com>
2022-04-23 12:25:00 +1000

    add create cell and wall methods

--- src/GridPanel.java
+++ src/GridPanel.java
@@ -5,11 +5,15 @@ import java.awt.*;
  * Creates the Grid Panel with editable cells for Maze construction
  */
 public class GridPanel extends JPanel {
+    enum Position {HORIZONTAL,VERTICAl}
+    private GridBagConstraints cst;
     private int mazeWidth;
     private int mazeHeight;
 
     public GridPanel() {
         super();
+        setLayout(new GridBagLayout());
+        cst = new GridBagConstraints();
     }
 
     /**
@@ -19,5 +23,33 @@ public class GridPanel extends JPanel {
      */
     protected void CreateGrid(int width, int height) {
 
+    }
+
+    /**
+     * Creates Empty JPanel Cell
+     * @return JPanel
+     */
+    private JPanel createCell() {
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(10,10));
+        return panel;
+    }
+
+    /**
+     * Creates Vertical or Horizontal JButton Walls
+     * @param position Vertical or Horizontal decides dimensions of button
+     * @return JButton
+     */
+    private JButton createWall(Position position) {
+        JButton btn = new JButton();
+        switch (position) {
+            case HORIZONTAL:
+                btn.setPreferredSize(new Dimension(10,2));
+                break;
+            case VERTICAl:
+                btn.setPreferredSize(new Dimension(2,10));
+                break;
+        }
+        return btn;
     }
 }
