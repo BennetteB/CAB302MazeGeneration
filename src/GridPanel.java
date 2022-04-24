1c99c6961249ff12cb77d1aa3c61efc088c2b48c
BennetteB <bennette.benoy@outlook.com>
2022-04-24 18:32:37 +1000

    finish grid manipulation functionality

--- src/GridPanel.java
+++ src/GridPanel.java
@@ -14,12 +14,16 @@ import java.util.ArrayList;
 /**
  * Creates the Grid Panel with editable cells for Maze construction
  */
-public class GridPanel extends JLayeredPane {
+public class GridPanel extends JPanel {
     private int sizeMultiplier = 5;
-    private Color GridColor = Color.WHITE;
-    enum Position {HORIZONTAL,VERTICAl}
+    private Color GRIDCOLOR = Color.WHITE;
+    private Color ROLLOVERCOLOR = Color.RED;
+    private Color SELECTEDCOLOR = Color.BLACK;
+    enum Orientation {HORIZONTAL,VERTICAl}
+    private final int CELLWIDTH = 10;
+    private final int WALLSHORT = 2;
     private GridBagConstraints cst;
-    private ArrayList<JToggleButton> btnList = new ArrayList<JToggleButton>();
+    private Component[][] GridComponentArray;
 
     public GridPanel() {
         super();
@@ -37,37 +41,28 @@ public class GridPanel extends JLayeredPane {
      * @param height Number of cells high
      */
     protected void CreateGrid(int width, int height) {
+        GridComponentArray = new Component[(height * 2) + 1][(width * 2) + 1];
         int x = 0;
         int y = 0;
         for(int i = 0; i < (height * 2) + 1; i++) {
             for(int j = 0; j < (width * 2) + 1; j++) {
                 if(i % 2 == 0) {
-                    if(j == 0){
-                        continue;
-                    }
-                    cst.gridx = x;
-                    cst.gridy = y;
-                    cst.gridwidth = 14;
-                    cst.gridheight = 2;
-                    add(createWall(Position.HORIZONTAL),cst,1);
-                    x += 12;
-                    j += 1;
-                }
-                else{
                     if(j % 2 == 0) {
-                        cst.gridx = x;
-                        cst.gridy = y - 2;
-                        cst.gridwidth = 2;
-                        cst.gridheight = 14;
-                        add(createWall(Position.VERTICAl),cst,1);
+                        createIntersect(x,y,i,j);
                         x += 2;
                     }
                     else {
-                        cst.gridx = x;
-                        cst.gridy = y;
-                        cst.gridwidth = 10;
-                        cst.gridheight = 10;
-                        add(createCell(),cst,1);
+                        createWall(x,y,Orientation.HORIZONTAL,i,j);
+                        x += 10;
+                    }
+                }
+                else{
+                    if(j % 2 == 0) {
+                        createWall(x,y,Orientation.VERTICAl,i,j);
+                        x += 2;
+                    }
+                    else {
+                        createCell(x,y,i,j);
                         x += 10;
                     }
                 }
@@ -83,55 +78,168 @@ public class GridPanel extends JLayeredPane {
     }
 
     /**
-     * Creates Empty JPanel Cell
-     * @return JPanel
+     * Creates empty panel and adds it to GridComponentArray and  panel
+     * @param x GridBagConstraints x position
+     * @param y GridBagConstraints y position
+     * @param i GridComponentArray x index
+     * @param j GridComponentArray y index
      */
-    private JPanel createCell() {
-        JPanel panel = new JPanel();
-        panel.setBackground(GridColor);
-        panel.setPreferredSize(new Dimension(10 * sizeMultiplier,10 * sizeMultiplier));
-        return panel;
+    private void createCell(int x, int y, int i, int j) {
+        Cell cell = new Cell(x,y,CELLWIDTH,i,j);
+        cell.setBackground(GRIDCOLOR);
+        cell.setPreferredSize(new Dimension(CELLWIDTH * sizeMultiplier,CELLWIDTH * sizeMultiplier));
+        GridComponentArray[i][j] = cell;
+        GridBagConstraints cst = new GridBagConstraints();
+        cst.gridx = x;
+        cst.gridy = y;
+        cst.gridwidth = CELLWIDTH;
+        cst.gridheight = CELLWIDTH;
+        add(GridComponentArray[i][j],cst);
     }
 
     /**
-     * Creates Vertical or Horizontal JButton Walls
-     * @param position Vertical or Horizontal decides dimensions of button
-     * @return JButton
+     * Creates small empty panel and adds it to GridComponentArray and panel
+     * @param x GridBagConstraints x position
+     * @param y GridBagConstraints y position
+     * @param i GridComponentArray x index
+     * @param j GridComponentArray y index
      */
-    private JToggleButton createWall(Position position) {
-        JToggleButton btn = new JToggleButton();
+    private void createIntersect(int x, int y, int i, int j) {
+        Cell cell = new Cell(x,y,WALLSHORT,i,j);
+        cell.setBackground(GRIDCOLOR);
+        cell.setPreferredSize(new Dimension(WALLSHORT * sizeMultiplier,WALLSHORT * sizeMultiplier));
+        GridComponentArray[i][j] = cell;
+        GridBagConstraints cst = new GridBagConstraints();
+        cst.gridx = x;
+        cst.gridy = y;
+        cst.gridwidth = WALLSHORT;
+        cst.gridheight = WALLSHORT;
+        add(GridComponentArray[i][j],cst);
+    }
+
+    /**
+     * Creates WallButton and adds it to GridComponentArray and panel, also assigns changeListener to Wallbutton for wall selection functionality
+     * @param x GridBagConstraints x position
+     * @param y GridBagConstraints y position
+     * @param orien The Orientation of the WallButton
+     * @param i GridComponentArray row
+     * @param j GridComponentArray column
+     */
+    private void createWall(int x, int y, Orientation orien, int i, int j) {
+        int width;
+        int height;
+        if(orien == Orientation.HORIZONTAL) {
+            width = CELLWIDTH;
+            height = WALLSHORT;
+        }else {
+            width = WALLSHORT;
+            height = CELLWIDTH;
+        }
+        WallButton btn = new WallButton(x,y,width,height,i,j);
         btn.setBorderPainted(false);
-        btn.setBackground(GridColor);
+        btn.setBackground(GRIDCOLOR);
         btn.setRolloverEnabled(true);
         btn.setUI(new MetalToggleButtonUI() {
             @Override
             protected Color getSelectColor() {
-                return Color.BLACK;
+                return SELECTEDCOLOR;
             }
         });
         btn.addChangeListener(new ChangeListener() {
             @Override
             public void stateChanged(ChangeEvent e) {
                 if(btn.getModel().isRollover()) {
-                    btn.setBackground(Color.RED);
+                    btn.setBackground(ROLLOVERCOLOR);
                 }
                 else{
-                    btn.setBackground(GridColor);
+                    btn.setBackground(GRIDCOLOR);
                 }
-                if(btn.isSelected()) {
+                if(btn.getModel().isSelected()) {
 
+                    if(btn.orientation == Orientation.HORIZONTAL) {
+                        GridComponentArray[btn.i][btn.j-1].setBackground(SELECTEDCOLOR);
+                        GridComponentArray[btn.i][btn.j+1].setBackground(SELECTEDCOLOR);
+                    }
+                    else {
+                        GridComponentArray[btn.i-1][btn.j].setBackground(SELECTEDCOLOR);
+                        GridComponentArray[btn.i+1][btn.j].setBackground(SELECTEDCOLOR);
+                    }
                 }
+                else {
+                    if(btn.orientation == Orientation.HORIZONTAL) {
+                        if(!isAdjacentToWall(btn.i,btn.j - 1)) {
+                            GridComponentArray[btn.i][btn.j - 1].setBackground(GRIDCOLOR);
+                        }
+                        if(!isAdjacentToWall(btn.i,btn.j + 1)) {
+                            GridComponentArray[btn.i][btn.j + 1].setBackground(GRIDCOLOR);
+                        }
+                    }
+                    else {
+                        if(!isAdjacentToWall(btn.i - 1,btn.j)) {
+                            GridComponentArray[btn.i - 1][btn.j].setBackground(GRIDCOLOR);
+                        }
+                        if(!isAdjacentToWall(btn.i + 1,btn.j)) {
+                            GridComponentArray[btn.i + 1][btn.j].setBackground(GRIDCOLOR);
+                        }
+                    }
+                }
+
             }
         });
-        switch (position) {
-            case HORIZONTAL:
-                btn.setPreferredSize(new Dimension(14 * sizeMultiplier,2 * sizeMultiplier));
-                break;
-            case VERTICAl:
-                btn.setPreferredSize(new Dimension(2 * sizeMultiplier,14 * sizeMultiplier));
-                break;
+        btn.setPreferredSize(new Dimension(width * sizeMultiplier, height * sizeMultiplier));
+        GridComponentArray[i][j] = btn;
+        cst.gridx = x;
+        cst.gridy = y;
+        cst.gridwidth = width;
+        cst.gridheight = height;
+        add(GridComponentArray[i][j],cst);
+    }
+
+    /**
+     * Checks if Intersect at provided coordinates has a selected wall next to it
+     * @param i
+     * @param j
+     * @return
+     */
+    private boolean isAdjacentToWall(int i, int j) {
+        int size =  GridComponentArray[0].length;
+        if(i == 0) {
+            if (j == 0) {
+                return ((WallButton) GridComponentArray[i][j+1]).getModel().isSelected() ||
+                        ((WallButton) GridComponentArray[i + 1][j]).getModel().isSelected();
+            } else if (j == GridComponentArray[0].length - 1) {
+                return ((WallButton) GridComponentArray[i][j-1]).getModel().isSelected() ||
+                        ((WallButton) GridComponentArray[i + 1][j]).getModel().isSelected();
+            } else {
+                return ((WallButton) GridComponentArray[i][j-1]).getModel().isSelected() ||
+                        ((WallButton) GridComponentArray[i][j+1]).getModel().isSelected() ||
+                        ((WallButton) GridComponentArray[i+1][j]).getModel().isSelected();
+            }
+        }else if(i == GridComponentArray.length - 1) {
+            if (j == 0) {
+                return ((WallButton) GridComponentArray[i][j+1]).getModel().isSelected() ||
+                        ((WallButton) GridComponentArray[i-1][j]).getModel().isSelected();
+            } else if (j == GridComponentArray[0].length - 1) {
+                return ((WallButton) GridComponentArray[i][j-1]).getModel().isSelected() ||
+                        ((WallButton) GridComponentArray[i-1][j]).getModel().isSelected();
+            } else {
+                return ((WallButton) GridComponentArray[i][j-1]).getModel().isSelected() ||
+                        ((WallButton) GridComponentArray[i][j+1]).getModel().isSelected() ||
+                        ((WallButton) GridComponentArray[i-1][j]).getModel().isSelected();
+            }
+        }else if(j == 0) {
+            return ((WallButton) GridComponentArray[i][j+1]).getModel().isSelected() ||
+                    ((WallButton) GridComponentArray[i-1][j]).getModel().isSelected() ||
+                    ((WallButton) GridComponentArray[i+1][j]).getModel().isSelected();
+        }else if(j == GridComponentArray[0].length - 1) {
+            return ((WallButton) GridComponentArray[i][j-1]).getModel().isSelected() ||
+                    ((WallButton) GridComponentArray[i-1][j]).getModel().isSelected() ||
+                    ((WallButton) GridComponentArray[i+1][j]).getModel().isSelected();
         }
-        btnList.add(btn);
-        return btnList.get(btnList.size() - 1);
+        return ((WallButton) GridComponentArray[i][j-1]).getModel().isSelected() ||
+                ((WallButton) GridComponentArray[i][j+1]).getModel().isSelected() ||
+                ((WallButton) GridComponentArray[i-1][j]).getModel().isSelected() ||
+                ((WallButton) GridComponentArray[i+1][j]).getModel().isSelected();
     }
 }
+
