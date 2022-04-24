fbf03476f2e90a476239e7b30e6c68892d316db7
sophiavan <sophiavan783@gmail.com>
2022-04-24 21:34:58 +1000

    updated left sidebar

--- src/LeftSideBarPanel.java
+++ src/LeftSideBarPanel.java
@@ -11,8 +11,8 @@ public class LeftSideBarPanel extends JPanel implements ActionListener {
      * properties of the left sidebar panel, which includes
      * width, height, base panel, and inner panel buttons.
      */
-    public static final int WIDTH = 300;
-    public static final int HEIGHT = 200;
+    public static final int WIDTH = 100;
+    public static final int HEIGHT = 100;
     private JPanel leftsidePanel;
     private JButton mazeStatsButton, editButton, solvableButton;
 
@@ -33,14 +33,16 @@ public class LeftSideBarPanel extends JPanel implements ActionListener {
         leftsidePanel = new JPanel();
         leftsidePanel.setBackground(Color.LIGHT_GRAY);
 
-        mazeStatsButton = new JButton("Maze Stats");
-        editButton = new JButton("Edit");
-        solvableButton = new JButton("Solvable");
-        mazeStatsButton.setBackground(Color.GRAY);
-        editButton.setBackground(Color.GRAY);
-        solvableButton.setBackground(Color.GRAY);
+        mazeStatsButton = new JButton("  Maze Stats  ");
+        editButton = new JButton("        Edit        ");
+        solvableButton = new JButton("   Solvable     ");
+        mazeStatsButton.setBackground(Color.WHITE);
 
-        leftsidePanel.setLayout(new FlowLayout());
+        editButton.setBackground(Color.WHITE);
+        solvableButton.setBackground(Color.WHITE);
+
+        leftsidePanel.setLayout(new BoxLayout(leftsidePanel, BoxLayout.Y_AXIS));
+        leftsidePanel.setAlignmentX(Component.CENTER_ALIGNMENT);
         leftsidePanel.add(mazeStatsButton);
         leftsidePanel.add(editButton);
         leftsidePanel.add(solvableButton);
