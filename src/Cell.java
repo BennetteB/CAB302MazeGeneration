4d693590876bb9c2124b46f53832d8cf8e033bb8
Andrew <andrewknott@hotmail.com.au>
2022-04-25 17:47:41 +0800

    Changed properties to protected

--- src/Cell.java
+++ src/Cell.java
@@ -4,11 +4,11 @@ import javax.swing.*;
  * Cell Class represents an empty space surrounded by walls
  */
 public class Cell extends JPanel {
-    private int x;
-    private int y;
-    private int width;
-    private int i;
-    private int j;
+    protected int x;
+    protected int y;
+    protected int width;
+    protected int i;
+    protected int j;
 
     public Cell(int x, int y, int width, int i, int j) {
         this.x = x;
